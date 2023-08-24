9c5e53025bf628f3ea884ac9d43d31f43a7c35aa
Tharit Suebamornpimon <tharit.sue@wipay.co.th>
2023-08-24 11:34:55 +0700

    update gold module to add an argument for periods list in the class

--- src/zones/gold/gold_module.py
+++ src/zones/gold/gold_module.py
@@ -2,9 +2,10 @@ import pandas as pd
 import os
 
 class DataProcessor:
-    def __init__(self, source_folder_path, target_folder_path):
+    def __init__(self, source_folder_path, target_folder_path, periods=[50, 100, 200]):
         self.source_folder_path = source_folder_path
         self.target_folder_path = target_folder_path
+        self.periods = periods
         pd.set_option("display.max_columns", 100)
 
     def process_data(self):
@@ -13,7 +14,7 @@ class DataProcessor:
         for csv_file in csv_files:
             csv_filename = csv_file.split(".csv")[0]
             df_temp = pd.read_csv(os.path.join(self.source_folder_path, csv_file))
-            df_ma = self.calculate_moving_averages(df_temp, [50, 100, 200])
+            df_ma = self.calculate_moving_averages(df_temp, self.periods)
 
             target_path = os.path.join(self.target_folder_path, csv_file)
             df_ma.to_csv(target_path)
@@ -23,6 +24,6 @@ class DataProcessor:
         df_ma = df.copy()
 
         for period in periods:
-            df_ma[f'ma{period}'] = df_ma['close'].rolling(window=period).mean()
+            df_ma[f'ma{period}'] = df_ma['close'].rolling(window=period, min_periods=1).mean()
 
         return df_ma
