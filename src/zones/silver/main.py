ec9de46189c3e20fd4b50ed6da5c4240438b1db2
Tharit Suebamornpimon <tharit.sue@wipay.co.th>
2023-08-24 09:21:24 +0700

    update main and module in silver zone

--- src/zones/silver/main.py
+++ src/zones/silver/main.py
@@ -1,15 +1,29 @@
 from silver_module import DataProcessor
+from pathlib import Path
 import os
 
 
 MAIN_FOLDER = "datalakehouse"
 SOURCE_ZONE_FOLDER = "bronze"
 INGESTION_FOLDER = "batch"
-
 TARGET_ZONE_FOLDER = "silver"
 
-data_processor = DataProcessor(
-    source_folder=os.path.join(MAIN_FOLDER, SOURCE_ZONE_FOLDER, INGESTION_FOLDER),
-    target_folder=os.path.join(MAIN_FOLDER, TARGET_ZONE_FOLDER, INGESTION_FOLDER)
+source_folder_path = os.path.join(
+    Path(__file__).parents[3],
+    MAIN_FOLDER,
+    SOURCE_ZONE_FOLDER,
+    INGESTION_FOLDER
 )
+
+target_folder_path = os.path.join(
+    Path(__file__).parents[3],
+    MAIN_FOLDER,
+    TARGET_ZONE_FOLDER,
+    INGESTION_FOLDER
+)
+
+data_processor = DataProcessor(
+    source_folder_path=source_folder_path, 
+    target_folder_path=target_folder_path
+    )
 data_processor.process_data()
