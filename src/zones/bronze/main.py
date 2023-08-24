d0a4e53dc99042fc77b1bc3c30fc3127fa9fcf35
Tharit Suebamornpimon <tharit.sue@wipay.co.th>
2023-08-24 09:20:54 +0700

    update main and module in brozne zone

--- src/zones/bronze/main.py
+++ src/zones/bronze/main.py
@@ -1,9 +1,27 @@
-from bronze_module import FullLoadDataIngestion
+from bronze_module import DataIngestion
+from pathlib import Path
+import os
 
 
-if __name__ == "__main__":
+symbols = ["AAVEUSDT", "STXUSDT", "ARBUSDT"]
+limit = 1000
 
-    symbols = ["AAVEUSDT", "STXUSDT", "ARBUSDT"]
+MAIN_FOLDER = "datalakehouse"
+INGESTION_FOLDER = "batch"
+TARGET_ZONE_FOLDER = "bronze"
 
-    for symbol in symbols:
-        FullLoadDataIngestion(symbol=symbol, limit=1000).write_trade_data_to_json()
+target_folder_path = os.path.join(
+    Path(__file__).parents[3],
+    MAIN_FOLDER,
+    TARGET_ZONE_FOLDER,
+    INGESTION_FOLDER
+)
+
+for symbol in symbols:
+    data_ingestion = DataIngestion(
+        symbol=symbol, 
+        limit=limit, 
+        target_folder_path=target_folder_path
+        )
+    data_ingestion.write_trade_data_to_json()
+
